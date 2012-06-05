e60161c47f9cd0a188854ab2b461866a70389b52
Roger Stringer <roger@freekrai.net>
2012-06-05 14:58:34 -0700

    sample table

--- sections/visitor/table.php
+++ sections/visitor/table.php
@@ -5,134 +5,39 @@
 		</div>
 		<div class="row">
 			<div class="span12">
-				<div id="datatable" class="advancedsort">
+				<div id="datatable">
 					<table width=100% class="table table-striped table-bordered">
 					<thead>
 					<tr>
-						<th>adfasdf</th>
-						<th>adsfafsd</th>
+						<th></th>
+						<th colspan="2">Full name</th>
+						<th></th>
+					</tr>
+					<tr>
+						<th>#</th>
+						<th>First Name</th>
+						<th>Last Name</th>
+						<th>Username</th>
 					</tr>
 					</thead>
 					<tbody>
+					<tr></tr>
 					<tr>
-						<td>3 afdsadsfas</td>
-						<td>adfadfs</td>
+						<td>1</td>
+						<td>Mark</td>
+						<td>Otto</td>
+						<td>@mdo</td>
 					</tr>
 					<tr>
-						<td>5 afdsadsfas</td>
-						<td>adfadfs</td>
+						<td>2</td>
+						<td>Jacob</td>
+						<td>Thornton</td>
+						<td>@fat</td>
 					</tr>
 					<tr>
-						<td>6 afdsadsfas</td>
-						<td>adfadfs</td>
-					</tr>
-					<tr>
-						<td>1 afdsadsfas</td>
-						<td>adfadfs</td>
-					</tr>
-					<tr>
-						<td>2 afdsadsfas</td>
-						<td>adfadfs</td>
-					</tr>
-					<tr>
-						<td>3 afdsadsfas</td>
-						<td>adfadfs</td>
-					</tr>
-					<tr>
-						<td>5 afdsadsfas</td>
-						<td>adfadfs</td>
-					</tr>
-					<tr>
-						<td>6 afdsadsfas</td>
-						<td>adfadfs</td>
-					</tr>
-					<tr>
-						<td>1 afdsadsfas</td>
-						<td>adfadfs</td>
-					</tr>
-					<tr>
-						<td>2 afdsadsfas</td>
-						<td>adfadfs</td>
-					</tr>
-					<tr>
-						<td>3 afdsadsfas</td>
-						<td>adfadfs</td>
-					</tr>
-					<tr>
-						<td>5 afdsadsfas</td>
-						<td>adfadfs</td>
-					</tr>
-					<tr>
-						<td>6 afdsadsfas</td>
-						<td>adfadfs</td>
-					</tr>
-					<tr>
-						<td>1 afdsadsfas</td>
-						<td>adfadfs</td>
-					</tr>
-					<tr>
-						<td>2 afdsadsfas</td>
-						<td>adfadfs</td>
-					</tr>
-					<tr>
-						<td>3 afdsadsfas</td>
-						<td>adfadfs</td>
-					</tr>
-					<tr>
-						<td>5 afdsadsfas</td>
-						<td>adfadfs</td>
-					</tr>
-					<tr>
-						<td>6 afdsadsfas</td>
-						<td>adfadfs</td>
-					</tr>
-					<tr>
-						<td>1 afdsadsfas</td>
-						<td>adfadfs</td>
-					</tr>
-					<tr>
-						<td>2 afdsadsfas</td>
-						<td>adfadfs</td>
-					</tr>
-					<tr>
-						<td>3 afdsadsfas</td>
-						<td>adfadfs</td>
-					</tr>
-					<tr>
-						<td>5 afdsadsfas</td>
-						<td>adfadfs</td>
-					</tr>
-					<tr>
-						<td>6 afdsadsfas</td>
-						<td>adfadfs</td>
-					</tr>
-					<tr>
-						<td>1 afdsadsfas</td>
-						<td>adfadfs</td>
-					</tr>
-					<tr>
-						<td>2 afdsadsfas</td>
-						<td>adfadfs</td>
-					</tr>
-					<tr>
-						<td>3 afdsadsfas</td>
-						<td>adfadfs</td>
-					</tr>
-					<tr>
-						<td>5 afdsadsfas</td>
-						<td>adfadfs</td>
-					</tr>
-					<tr>
-						<td>6 afdsadsfas</td>
-						<td>adfadfs</td>
-					</tr>
-					<tr>
-						<td>1 afdsadsfas</td>
-						<td>adfadfs</td>
-					</tr>
-					<tr>
-						<td>2 afdsadsfas</td>
-						<td>adfadfs</td>
+						<td>3</td>
+						<td colspan="2">Larry the Bird</td>
+						<td>@twitter</td>
 					</tr>
 					</tbody>
 					</table>
